a401db73c992a1ca4256769916ce7ffd1a632c19
Dang Van Thinh <dangvanthinh372004@gmail.com>
2023-10-12 16:21:56 +0700

    update contact view

--- view/client/lienhe.php
+++ view/client/lienhe.php
@@ -1,3 +1,37 @@
 <div class="margin_top">
-    liên hệ
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="border border-success p-3 my-3">
+                    <h3 class="text-center">Phản hồi</h3>
+                    <form action="" method="post">
+                        <div>
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" name="email" id="email" placeholder="Nhập email liên hệ" class="form-control">
+                        </div>
+                        <div>
+                            <label for="noidung_lh" class="form-label">Nội dung</label>
+                            <textarea name="noidung_lh" id="noidung_lh" cols="30" rows="10" placeholder="Nhập nội dung " class="form-control"></textarea>
+                        </div>
+                        <div class="mt-3">
+                            <input type="submit" value="Gửi" name="lh" class="btn btn-outline-success">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h3 class="text-center">Về chúng tôi</h3>
+                <p>Anh Hòa Bakery là thương hiệu bánh ngọt Pháp của công ty cổ phần bánh ngọt Anh Hòa. 
+                    Được thành lập từ năm 2004 tại con phố Ngõ Trạm, quận Hoàn Kiếm, Hà Nội. Trải qua hơn 15 năm phát triển, đến nay Anh Hòa Bakery đã có 13 cơ sở kinh doanh đặt trên những tuyến phố đông dân cư ở Hà Nội. 
+                    Các sản phẩm Anh Hòa Bakery được làm từ các nguyên liệu nhập khẩu của các nước có truyền thống làm bánh như: Newzeland, Mỹ, Pháp, Bỉ. Với hương vị thơm ngon đặc trưng của các loại kem, bơ, sữa, phô mai, hạt hạnh nhân, chocolate... dưới bàn tay khéo léo của những người thợ làm bánh giàu kinh nghiệm. 
+                    Quy mô xưởng sản xuất rộng hơn 2000m2 với các thiết bị tiên tiến hiện đại theo tiêu chuẩn ISO 2018, toàn bộ nhà máy được sơn phủ bởi sơn EPOXY đặc biệt.
+                     Anh Hòa Bakery luôn mang đến cho khách hàng những sản phẩm chất lượng nhất, đảm bảo tuyệt đối về an toàn vệ sinh thực phẩm.</p>
+            </div>
+        </div>
+        <div class="my-3 text-center">
+            <a href="https://www.google.com/maps/">
+                <img src="../view/assets/img/customize/map.png" alt="map">
+            </a>
+        </div>
+    </div>
 </div>
